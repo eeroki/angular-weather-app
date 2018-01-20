c96d7c1edf0ef30d3f612d9a774c8237a1dbd27a
Eero Kirjavainen <eKirjavainen@gmail.com>
2018-01-20 16:42:28 +0200

    Search for forecasts by city

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { WeatherInfo } from './weather/weather-info';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { exampleForecasts } from './weather/example-forecasts';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +15,17 @@ export class AppComponent implements OnInit {
 
   title = 'Weather app';
   weatherInfo: Observable<WeatherInfo>;
+  searchForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.searchForm = fb.group({
+      term: [undefined]
+    });
+  }
 
   ngOnInit(): void {
-    this.weatherInfo = Observable.of(exampleForecasts);
+    this.weatherInfo = this.searchForm.get('term').valueChanges
+    .debounceTime(300)
+    .switchMap(term => Observable.of(exampleForecasts));
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { AppComponent } from './app.component';
+import { WeatherForecastModule } from './weather/weather-forecast.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { registerLocaleData } from '@angular/common';
 import localeFi from '@angular/common/locales/fi';
@@ -12,7 +14,10 @@ registerLocaleData(localeFi, 'fi-FI');
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,    
+    ReactiveFormsModule,
+    WeatherForecastModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'fi-FI' }
